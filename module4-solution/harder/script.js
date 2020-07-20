3c03b8386a37c32f4e2131ec3a2b1a0fbf799c8a
johanm15 <johanm325@gmail.com>
2020-07-20 16:42:37 -0500

    update

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -52,17 +52,17 @@ WARNING!!! WARNING!!!
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-     var firstLetter = names[i].charAt();
+     var firstLetter = names[i].charAt(0).toLowerCase();
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (firstLetter.toLowerCase = 'j') {
-      byeSpeaker.speak();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
     } else {
-      helloSpeaker.speak();
+      helloSpeaker.speak(names[i]);
     }
   }
 })();
